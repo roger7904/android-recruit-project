d85b015d35cf096bd81dbb47701b4447670599ce
roger <rogerchang7904@gmail.com>
2023-04-19 21:26:07 +0800

    import landscapist to load image

--- app/src/main/java/in/hahow/android_recruit_project/ui/home/HomeScreen.kt
+++ app/src/main/java/in/hahow/android_recruit_project/ui/home/HomeScreen.kt
@@ -1,15 +1,23 @@
 package `in`.hahow.android_recruit_project.ui.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Gray
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.skydoves.landscapist.glide.GlideImage
 import `in`.hahow.android_recruit_project.ui.theme.HahowTheme
 
 @Composable
@@ -24,16 +32,32 @@ fun HomeScreen(
     ) {
         when (homeUiState) {
             is HomeUiState.Loading -> {
-                  item {
-                      Text(
-                          text = "Loading",
-                          color = MaterialTheme.colorScheme.onBackground
-                      )
-                 }
+                item {
+                    Box(modifier = Modifier.fillParentMaxSize()) {
+                        CircularProgressIndicator(
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                    }
+                }
             }
             is HomeUiState.Success -> {
                 items((homeUiState as HomeUiState.Success).courses.data) { course ->
-                    Greeting(course.title)
+                    GlideImage(
+                        modifier = modifier,
+                        imageModel = { course.coverImageUrl },
+                        loading = {
+                            Box(
+                                modifier = Modifier.background(
+                                    brush = Brush.linearGradient(
+                                        listOf(Gray, Color.White)
+                                    )
+                                )
+                            )
+                        },
+                        failure = {
+                            Text(text = "image request failed.")
+                        }
+                    )
                 }
             }
             is HomeUiState.Error -> {
