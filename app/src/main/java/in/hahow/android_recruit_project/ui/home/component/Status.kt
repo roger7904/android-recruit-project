6cf02ed8f22647b91079d16f887c559225dbb763
roger <rogerchang7904@gmail.com>
2023-04-22 16:15:51 +0800

    implement status preview

--- app/src/main/java/in/hahow/android_recruit_project/ui/home/component/Status.kt
+++ app/src/main/java/in/hahow/android_recruit_project/ui/home/component/Status.kt
@@ -1,5 +1,6 @@
 package `in`.hahow.android_recruit_project.ui.home.component
 
+import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -9,12 +10,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import `in`.hahow.android_recruit_project.ui.theme.HahowTheme
 
 @Composable
 fun Status(
-    modifier: Modifier,
+    modifier: Modifier = Modifier,
     content: String,
     backgroundColor: Color,
     textColor: Color,
@@ -31,3 +34,30 @@ fun Status(
         style = MaterialTheme.typography.bodySmall
     )
 }
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, showBackground = true)
+@Composable
+fun StatusPreview() {
+    HahowTheme {
+        Status(
+            content = "Preview Status",
+            backgroundColor = Color(0xFFF2AA63),
+            textColor = MaterialTheme.colorScheme.background,
+            cornerSize = 10.dp
+        )
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun StatusPreviewDark() {
+    HahowTheme {
+        Status(
+            content = "Preview Status",
+            backgroundColor = Color(0xFFF2AA63),
+            textColor = MaterialTheme.colorScheme.background,
+            cornerSize = 10.dp
+        )
+    }
+}
+
